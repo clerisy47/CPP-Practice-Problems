dbf53200308731ab96a4217f8a03f5820d83b08f
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-12 16:24:19 +0545

    add new problem

---
+++ 1 Basics/5_inline_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+inline int netPayment(int salary)
+{
+    return salary - salary * 0.1;
+}
+
+int main()
+{
+    int salary;
+    cin >> salary;
+    cout<<"Salary after tax deduction "<<netPayment(salary)<<endl;
+    return 0;
+}
