a30925f2774311dd25ce29232b8e17028c0d0254
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-12 16:24:12 +0545

    add new problem

---
+++ 1 Basics/3_namespace.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+namespace Square
+{
+    int num;
+    int fun(int num)
+    {
+        return num * num;
+    }
+}
+
+namespace Cube
+{
+    int num;
+    int fun(int num)
+    {
+        return num * num * num;
+    }
+}
+
+int main()
+{
+    cin >> Square::num;
+    cin >> Cube::num;
+    cout << Square::fun(Cube::num) << endl;
+    cout << Cube::fun(Square::num);
+    return 0;
+}
