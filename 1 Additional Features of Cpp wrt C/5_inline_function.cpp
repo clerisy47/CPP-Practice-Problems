f760241a84a8d352ea0b1d1b07640f6a575563ee
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-26 11:41:33 +0545

    add new program

---
+++ 1 Additional Features of Cpp wrt C/5_inline_function.cpp
@@ -0,0 +1,17 @@
+// inline function example
+
+#include <iostream>
+using namespace std;
+
+inline int netPayment(int salary)
+{
+    return salary - salary * 0.1;
+}
+
+int main()
+{
+    int salary;
+    cin >> salary;
+    cout<<"Salary after tax deduction "<<netPayment(salary)<<endl;
+    return 0;
+}
