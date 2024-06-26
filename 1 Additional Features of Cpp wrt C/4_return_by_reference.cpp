f671678a06eb274d06f6f4e3c8e2acb000faf968
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-26 11:41:36 +0545

    add new program

---
+++ 1 Additional Features of Cpp wrt C/4_return_by_reference.cpp
@@ -0,0 +1,21 @@
+// Returning by reference
+
+#include <iostream>
+using namespace std;
+
+int &larger(int &a, int &b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    larger(a, b) = 100;
+    cout << "After changing value of larger " << a << " " << b << endl;
+}
