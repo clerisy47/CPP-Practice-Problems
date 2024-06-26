2438ee76aef3686f6aea1c37ecfc080af038c89e
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-26 11:41:38 +0545

    add new program

---
+++ 1 Additional Features of Cpp wrt C/6_default_arguments.cpp
@@ -0,0 +1,19 @@
+// default argument example
+
+#include <iostream>
+using namespace std;
+
+int increment(int salary, int increment = 12)
+{
+    return salary + salary * increment / 100;
+}
+
+int main()
+{
+    cout << "After increment salary of" << endl;
+    cout << "CEO " << increment(35000, 9) << endl;
+    cout << "Information Officer " << increment(25000, 10) << endl;
+    cout << "Chief system analyst " << increment(24000) << endl;
+    cout << "Chief programmer " << increment(18000) << endl;
+    return 0;
+}
