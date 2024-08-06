1b3fd2d946ee0d0e28fce1cfabd10e69ba17c7f5
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-08-06 08:26:30 +0545

    style: formatting

--- 1 Additional Features of Cpp wrt C/4_return_by_reference.cpp
+++ 1 Additional Features of Cpp wrt C/4_return_by_reference.cpp
@@ -1,11 +1,11 @@
-// Returning by reference
+// Program that inputs two integers and sets the smallest integer to the average between them
 
 #include <iostream>
 using namespace std;
 
 int &larger(int &a, int &b)
 {
-    if (a > b)
+    if (a < b)
     {
         return a;
     }
@@ -16,6 +16,6 @@ int main()
 {
     int a, b;
     cin >> a >> b;
-    larger(a, b) = 100;
+    larger(a, b) = (a + b) / 2;
     cout << "After changing value of larger " << a << " " << b << endl;
 }
