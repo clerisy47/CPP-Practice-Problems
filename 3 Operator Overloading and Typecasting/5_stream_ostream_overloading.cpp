d491a87645ff275bbd32998b454d7e4497c47289
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-08-07 08:29:41 +0545

    feat: add new program

---
+++ 3 Operator Overloading and Typecasting/5_stream_ostream_overloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+private:
+    string name;
+    int age;
+
+public:
+    // must use non-member function
+    friend ostream &operator<<(ostream &os, Person &p)
+    {
+        os << p.name << " " << p.age;
+        return os;
+    }
+
+    // must use non-member function
+    friend istream &operator>>(istream &is, Person &p)
+    {
+        is >> p.name >> p.age;
+        return is;
+    }
+};
+
+int main()
+{
+    Person P;
+    cin >> P;
+    cout << P << endl;
+}
