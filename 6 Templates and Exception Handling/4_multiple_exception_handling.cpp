aabea32c1cc5f44bafb62c62e9704548c7e5aec5
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-08-29 16:49:59 +0545

    feat:add new problem

---
+++ 6 Templates and Exception Handling/4_multiple_exception_handling.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 2;
+
+template <class T>
+class Stack
+{
+private:
+    T arr[MAX];
+    int top;
+
+public:
+    Stack()
+    {
+        top = -1;
+    }
+    class Full
+    {
+    };
+    class Empty
+    {
+    };
+    void push(T x)
+    {
+        if (top == MAX - 1)
+        {
+            throw Full();
+        }
+        else
+        {
+            arr[++top] = x;
+        }
+    }
+    T pop()
+    {
+        if (top == -1)
+        {
+            throw Empty();
+        }
+        else
+        {
+            return arr[top--];
+        }
+    }
+};
+
+int main()
+{
+    Stack<int> s;
+    try
+    {
+        s.push(5);
+        s.push(1);
+        // s.push(5); // Error: stack is full
+        cout << s.pop() << endl;
+        cout << s.pop() << endl;
+        // cout << s.pop() << endl; // Error: stack is empty
+    }
+    catch (Stack<int>::Full f)
+    {
+        cout << "Error: Stack is full";
+    }
+    catch (Stack<int>::Empty e)
+    {
+        cout << "Error: Stack is empty";
+    }
+}
