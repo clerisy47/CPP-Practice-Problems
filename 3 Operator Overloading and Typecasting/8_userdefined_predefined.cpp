04d6504fc52006e1d0cba59fe4980623866556ff
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-08-31 13:37:37 +0545

    chore: rename

--- 3 Operator Overloading and Typecasting/8_userdefined_predefined.cpp
+++ 3 Operator Overloading and Typecasting/8_userdefined_predefined.cpp
@@ -4,17 +4,17 @@
 #include <cmath>
 using namespace std;
 
-class Round
+class MyRound
 {
 private:
     float f;
 
 public:
-    Round(float f) : f(f) {}
+    MyRound(float f) : f(f) {}
     // unlike operator overloading it doesn't need friend return type infront of operator
     operator int()
     {
-        return (int)round(f);
+        return static_cast<int>(round(f));
     }
 };
 
@@ -22,6 +22,6 @@ int main()
 {
     float f;
     cin >> f;
-    Round r = 5;            // equivalent to Round r(5) ie typecasting from float to Round, which is done automatically,
-    cout << (int)r << endl; // typecasting from Round to int which need to be done throught operator overloading as see in Round class
+    MyRound r = f;                       // Predefined to userdefined. Equivalent to MyRound r(5). Done implicitly.
+    cout << static_cast<int>(r) << endl; // Userdefined to predifined. Need to overload operator int()
 }
