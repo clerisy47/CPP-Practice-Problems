d7f026022e51f09096bce64ca83d41a253bff133
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-09-09 20:15:30 +0545

    refractor

--- 3 Operator Overloading and Typecasting/2_year_month_day.cpp
+++ 3 Operator Overloading and Typecasting/2_year_month_day.cpp
@@ -15,27 +15,26 @@ public:
         cout << year << "/" << month << "/" << day << endl;
     }
     // adds two dates
-    friend Date operator+(Date d1, Date d2)
+    friend Date operator+(Date &d1, Date &d2)
     {
         Date d3(d1.year + d2.year, d1.month + d2.month, d1.day + d2.day);
-        if (d3.day > 30) // if condition is most cause without it we will encounter 0 based indexing error
+        if (d3.day > 30)
         {
-            d3.month += d3.day / 30;
-            d3.day = d3.day % 30;
+            d3.month++;
+            d3.day %= 30;
         }
-        if (d3.day > 12)
+        if (d3.month > 12)
         {
-            d3.year += d3.month / 12;
-            d3.month = d3.month % 12;
+            d3.year++;
+            d3.month %= 12;
         }
-
         return d3;
     }
     // finds number of days between two dates
-    friend int operator-(Date D1, Date D2)
+    friend int operator-(Date d1, Date d2)
     {
-        int days1 = D1.year * 365 + D1.month * 12 + D1.day;
-        int days2 = D2.year * 365 + D2.month * 12 + D2.day;
+        int days1 = d1.year * 365 + d1.month * 12 + d1.day;
+        int days2 = d2.year * 365 + d2.month * 12 + d2.day;
         return days1 - days2;
     }
 };
