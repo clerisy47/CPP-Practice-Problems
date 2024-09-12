02a192914cb137118dbb2e9a10d8a73bbddccd2c
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-09-12 21:12:28 +0545

    refractor

--- 3 Operator Overloading and Typecasting/6_string_concatenation.cpp
+++ 3 Operator Overloading and Typecasting/6_string_concatenation.cpp
@@ -33,8 +33,20 @@ public:
     friend MyString operator+(MyString &s1, MyString &s2)
     {
         MyString result;
-        result.str = new char[strlen(s1.str), strlen(s2.str)];
-        strcpy(result.str, strcat(s1.str, s2.str));
+        int len1 = strlen(s1.str);
+        int len2 = strlen(s2.str);
+        result.str = new char[len1 + len2 + 1]; // +1 for the null terminator
+
+        for (int i = 0; i < len1; ++i)
+        {
+            result.str[i] = s1.str[i];
+        }
+        for (int i = 0; i < len2; ++i)
+        {
+            result.str[len1 + i] = s2.str[i];
+        }
+        result.str[len1 + len2] = '\0';
+
         return result;
     }
 };
